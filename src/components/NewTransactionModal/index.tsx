67a55b83945fc41eec1bd41d9f54e19a3b6ee7af
GustavoTeo <62210677+GustavoTeo@users.noreply.github.com>
2022-03-06 18:52:48 -0300

    Add form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
 import {Container} from './styles';
+import closeImg from '../../assets/close.svg';
 
 interface NewTransactionModal {
     isOpen: boolean;
@@ -14,6 +15,9 @@ export function NewTransactionModal({isOpen, onRequestClose }: NewTransactionMod
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
       >
+        <button type="button" onClick={onRequestClose} className="react-modal-close">
+          <img src={closeImg} alt="Fechar modal" />  
+        </button>
       <Container>
         <h2>Cadastrar transação</h2>
         <input placeholder="Título" />
